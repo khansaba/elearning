eaff67a4424d9820ccd33ff72cc74a4a14b8db8b
saba <khansabamhs01@gmail.com>
2022-06-18 17:40:37 +0530

    Initial commit

---
+++ admin/home.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <div class="container">
+        <div class="team_container">
+            <div class="text">Dashboard</div>
+        </div>
+        <div class="row">
+            <!--First User design starts here-->
+            <div class="profile-card">
+                <div class="profile-content">
+                    <div class="profile-image">
+                        <img src="system.png" alt="first user">
+                    </div>
+                    <div class="btn-div">
+                        <a href="http://localhost/elearning/admin/?page=system_info">
+                        <button class="btn"><i class="fas fa-cog" style="padding-right: 10px;"></i>System</button>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <!--First user design ends here-->
+             <!--second User design starts here-->
+             <div class="profile-card">
+                <div class="profile-content">
+                    <div class="profile-image">
+                        <img src="profile.png" alt="first user">
+                    </div>                  
+                    <div class="btn-div">
+                        <a href="http://localhost/elearning/admin/?page=user">
+                        <button class="btn"><i class="nav-icon fas fa-list" style="padding-right: 10px;"></i>Profile</button>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <!--second user design ends here-->
+             <!--third User design starts here-->
+             <div class="profile-card">
+                <div class="profile-content">
+                    <div class="profile-image">
+                        <img src="student.jpg" alt="first user">
+                    </div>
+                   
+                    <div class="btn-div">
+                        <a href="http://localhost/elearning/admin/?page=students">
+                        <button class="btn"><i class="nav-icon fas fa-list" style="padding-right: 10px;"></i>Students</button>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <!--third user design ends here-->
+            <!--third User design starts here-->
+            <div class="profile-card">
+                <div class="profile-content">
+                    <div class="profile-image">
+                        <img src="faculty img.jpg" alt="first user">
+                    </div>
+                   
+                    <div class="btn-div">
+                        <a href="http://localhost/elearning/admin/?page=faculty">
+                        <button class="btn"><i class="nav-icon fas fa-list" style="padding-right: 10px;"></i>Faculty</button>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <!--third user design ends here-->
+            <!--third User design starts here-->
+            <div class="profile-card">
+                <div class="profile-content">
+                    <div class="profile-image">
+                        <img src="classs.png" alt="first user">
+                    </div>
+                   
+                    <div class="btn-div">
+                        <a href="http://localhost/elearning/admin/?page=class">
+                        <button class="btn"><i class="nav-icon fas fa-list" style="padding-right: 10px;"></i>Class</button>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <!--third user design ends here-->
+            <!--third User design starts here-->
+            <div class="profile-card">
+                <div class="profile-content">
+                    <div class="profile-image">
+                        <img src="departments.png" alt="first user">
+                    </div>
+                   
+                    <div class="btn-div">
+                        <a href="http://localhost/elearning/admin/?page=department">
+                        <button class="btn"><i class="nav-icon fas fa-list" style="padding-right: 10px;"></i>Department</button>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <!--third user design ends here-->
+            <!--third User design starts here-->
+            <div class="profile-card">
+                <div class="profile-content">
+                    <div class="profile-image">
+                        <img src="course1.png" alt="first user">
+                    </div>
+                   
+                    <div class="btn-div">
+                        <a href="http://localhost/elearning/admin/?page=course">
+                        <button class="btn"><i class="nav-icon fas fa-list" style="padding-right: 10px;"></i>Course</button>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <!--third user design ends here-->
+            <!--third User design starts here-->
+            <div class="profile-card">
+                <div class="profile-content">
+                    <div class="profile-image">
+                        <img src="subject1.png" alt="first user">
+                    </div>
+                   
+                    <div class="btn-div">
+                        <a href="http://localhost/elearning/admin/?page=subject">
+                        <button class="btn"><i class="nav-icon fas fa-list" style="padding-right: 10px;"></i>Subject</button>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <!--third user design ends here-->
+            <!--third User design starts here-->
+            <!--div class="profile-card">
+                <div class="profile-content">
+                    <div class="profile-image">
+                        <img src="lesson-icon-13.jpg" alt="first user">
+                    </div>
+                   
+                    <div class="btn-div">
+                        <a href="http://localhost/elearning/faculty/?page=lesson">
+                        <button class="btn"><i class="nav-icon fas fa-list" style="padding-right: 10px;"></i>Lesson</button>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <!--third user design ends here-->
+             
+        </div>
+    </div>
+</body>
+</html>
---
+++ student/home.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <div class="container">
+        <div class="team_container">
+            <div class="text">Dashboard</div>
+        </div>
+        <div class="row">
+            <!--First User design starts here-->
+            <div class="profile-card">
+                <div class="profile-content">
+                    <div class="profile-image">
+                        <img src="profile.png" alt="first user">
+                    </div>
+                    <div class="btn-div">
+                        <a href="http://localhost/elearning/student/?page=user">
+                        <button class="btn"><i class="nav-icon fas fa-users" style="padding-right: 10px;"></i>Profile</button>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <!--First user design ends here-->
+             <!--second User design starts here-->
+             <div class="profile-card">
+                <div class="profile-content">
+                    <div class="profile-image">
+                        <img src="subject1.png" alt="first user">
+                    </div>                  
+                    <div class="btn-div">
+                        <a href="http://localhost/elearning/student/?page=subject">
+                        <button class="btn"><i class="nav-icon fas fa-list" style="padding-right: 10px;"></i>Subject</button>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <!--second user design ends here-->
+             <!--third User design starts here-->
+             <div class="profile-card">
+                <div class="profile-content">
+                    <div class="profile-image">
+                        <img src="lesson-icon-13.jpg" alt="first user">
+                    </div>
+                   
+                    <div class="btn-div">
+                        <a href="http://localhost/elearning/student/?page=lesson">
+                        <button class="btn"><i class="nav-icon fas fa-list" style="padding-right: 10px;"></i>Lesson</button>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <!--third user design ends here-->
+             
+        </div>
+    </div>
+</body>
+</html>
